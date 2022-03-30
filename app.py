68bfe1be3f5b6d5f377a5aaf8766eaad284acbed
Vamsi Ravula <83864467+rpscodes@users.noreply.github.com>
2022-03-29 18:15:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def index():
-	return 'Hello Vamsi RH'
+	return 'Hello World'
 
 # main driver function
 if __name__ == '__main__':
